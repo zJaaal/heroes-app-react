8551c70ad8821c68c7b1078c1dba234674095112
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-29 12:22:30 -0300

    Initial commit

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,9 @@
+function HeroesApp() {
+  return (
+    <div>
+      <h3>Hello World</h3>
+    </div>
+  );
+}
+
+export default HeroesApp;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeroesApp from "./HeroesApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<HeroesApp />);
