d682d4b5a147ac66e69c7a924b6e99bb533ab219
Jalinson Diaz <zjaaaldev@gmail.com>
2022-06-29 20:07:45 -0300

    Create HeroesList.jsx
    Added heroes component to show heroes by publisher

---
+++ src/components/HeroesList.jsx
@@ -0,0 +1,57 @@
+import {
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import getHeroesByPublisher from "../actions/getHeroByPublisher";
+
+const HeroesList = ({ publisher }) => {
+  const [heroesList, setHeroesList] = useState([{}]);
+
+  useEffect(() => {
+    setHeroesList(getHeroesByPublisher(publisher));
+  }, []);
+
+  return (
+    <Grid
+      container
+      columnSpacing={2}
+      columnGap={0.4}
+      alignContent="center"
+      justifyContent="center"
+    >
+      {heroesList.map((hero) => {
+        return (
+          <Grid item xs={3} mt={2} key={hero.id}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="400"
+                src={`../../assets/${hero.id}.jpg`}
+                alt={`${hero.superhero}`}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {hero.superhero}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {`Literally ${hero.superhero} bro`}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default HeroesList;
