c2b8426bb09d05b1bfe688bb40db2f8920828821
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-01 20:39:25 -0300

    fixed

--- src/tests/auth/authReducer.test.js
+++
@@ -1,9 +0,0 @@
-import authReducer from "../../auth/authReducer.js";
-
-describe("Tests on authReducer", () => {
-  test("Should return the default state", () => {
-    const state = authReducer({ logged: false }, {});
-
-    expect(state).toEqual({ logged: false });
-  });
-});
